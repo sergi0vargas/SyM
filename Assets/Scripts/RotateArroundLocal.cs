c492062ca1b95e6d6839430428ddde12a2d30f0e
SERGIO\sergio <sergiovargas9@gmail.com>
2017-05-09 19:57:28 -0500

    Version 1.2

---
+++ Assets/Scripts/RotateArroundLocal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateArroundLocal : MonoBehaviour {
+
+    public float speed = 20f;
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0,speed * Time.deltaTime,0);
+	}
+
+    public void OpenURL(string direccion)
+    {
+        if (direccion != null)
+        {
+            Application.OpenURL(direccion);
+        }
+        else
+        {
+            Application.OpenURL("http://www.sym.com.co/");
+        }
+    }
+
+}
